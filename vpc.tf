1b8ab6383993a53fbd49f5267fe04749b221fbe0
Ajaylala6 <lalaajay0007@gmail.com>
2023-11-15 21:52:07 +0000

    create subnet

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,27 @@ resource "aws_vpc" "Tf-food" {
     Name = "Tf-food"
   }
 }
+
+
+##CREATE AWS-SUBNET-1
+resource "aws_subnet" "tf-food-subnet-1" {
+  vpc_id     = aws_vpc.Tf-food.id
+  cidr_block = "192.168.0.0/24"
+  availability_zone = "ap-south-1b"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "Tf-pub-sn"
+  }
+}
+
+###CREATE AWS-SUBNET-2
+resource "aws_subnet" "tf-food-subnet-2" {
+  vpc_id     = aws_vpc.Tf-food.id
+  cidr_block = "192.168.1.0/24"
+  availability_zone = "ap-south-1c"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "Tf-pub-sub-1"
+  }
+}
