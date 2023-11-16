0cd034974b61503fecf3640bd08699c423a9d54c
Ajaylala6 <lalaajay0007@gmail.com>
2023-11-16 00:56:25 +0000

    create sg

---
+++ ec2.tf
@@ -0,0 +1,35 @@
+# Security Group
+resource "aws_security_group" "tf-food-pub-sg" {
+  name        = "food-web-sg"
+  description = "Allow SSH & HTTP traffic"
+  vpc_id      = aws_vpc.Tf-food.id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "food-web-sg"
+  }
+}
+
+
